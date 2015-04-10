6b331594f622eea5cc9ae6e10e3e14794800ed05
Ben Page <bpage@dewalch.com>
2015-04-10 13:18:00 -0500

    rework connection creation to avoid blocking while a large number of connection are being created

--- test/connection-pool.test.js
+++ test/connection-pool.test.js
@@ -42,7 +42,7 @@ describe('ConnectionPool', function () {
         setTimeout(function() {
             assert.equal(pool.connections.length, poolConfig.min);
             pool.drain(done);
-        }, 4);
+        }, 100);
     });
 
     it('min=0', function (done) {
@@ -53,7 +53,7 @@ describe('ConnectionPool', function () {
 
         setTimeout(function() {
             assert.equal(pool.connections.length, 0);
-        }, 4);
+        }, 100);
 
         setTimeout(function() {
             pool.acquire(function(err, connection) {
@@ -110,74 +110,10 @@ describe('ConnectionPool', function () {
         };
 
         for (var i = 0; i < count; i++) {
-            setTimeout(function() {
+            setImmediate(function() {
                 pool.acquire(createRequest);
-            }, 1);
+            });
         }
-    });
-
-    it('min<=max, min specified > max specified', function (done) {
-        this.timeout(10000);
-
-        var poolConfig = { min: 5, max: 1, idleTimeout: 10};
-        var pool = new ConnectionPool(poolConfig, connectionConfig);
-
-        setTimeout(function() {
-            assert.equal(pool.connections.length, 1);
-        }, 4);
-
-        setTimeout(function() {
-            pool.acquire(function(err, connection) {
-                assert(!err);
-
-                var request = new Request('select 42', function (err, rowCount) {
-                    assert.strictEqual(rowCount, 1);
-                    connection.release();
-                    setTimeout(function () {
-                        assert.equal(pool.connections.length, 1);
-                        pool.drain(done);
-                    }, 200);
-                });
-
-                request.on('row', function (columns) {
-                    assert.strictEqual(columns[0].value, 42);
-                });
-
-                connection.execSql(request);
-            });
-        }, 2000);
-    });
-
-    it('min<=max, no min specified', function (done) {
-        this.timeout(10000);
-
-        var poolConfig = {max: 1, idleTimeout: 10};
-        var pool = new ConnectionPool(poolConfig, connectionConfig);
-
-        setTimeout(function() {
-            assert.equal(pool.connections.length, 1);
-        }, 4);
-
-        setTimeout(function() {
-            pool.acquire(function(err, connection) {
-                assert(!err);
-
-                var request = new Request('select 42', function (err, rowCount) {
-                    assert.strictEqual(rowCount, 1);
-                    connection.release();
-                    setTimeout(function () {
-                        assert.equal(pool.connections.length, 1);
-                        pool.drain(done);
-                    }, 200);
-                });
-
-                request.on('row', function (columns) {
-                    assert.strictEqual(columns[0].value, 42);
-                });
-
-                connection.execSql(request);
-            });
-        }, 2000);
     });
 
     it('pool error event', function (done) {
@@ -191,27 +127,26 @@ describe('ConnectionPool', function () {
 
     it('connection retry', function (done) {
         this.timeout(10000);
-        var poolConfig = {min: 1, max: 5, retryDelay: 5};
+        var poolConfig = {min: 3, max: 3, retryDelay: 2000 };
         var pool = new ConnectionPool(poolConfig, {});
 
+        var errors = 0;
+
         pool.on('error', function(err) {
-            assert(!!err);
-            pool.connectionConfig = connectionConfig;
-        });
+            if (++errors === 3) {
+                assert.equal(pool.connections.length, 0);
+                pool.connectionConfig = connectionConfig; //fix config so new connections succeed
 
-        function testConnected() {
-            for (var i = pool.connections.length - 1; i >= 0; i--) {
-                if (pool.connections[i].status === 3/*RETRY*/) {
-                    setTimeout(testConnected, 100);
-                    return;
-                }
+                setTimeout(function() {
+                    assert.equal(pool.connections.length, 0);
+                }, 1000);
+
+                setTimeout(function() {
+                    assert.equal(pool.connections.length, 3);
+                    done();
+                }, 3000)
             }
-
-            assert.equal(pool.connections.length, poolConfig.min);
-            pool.drain(done);
-        }
-
-        setTimeout(testConnected, 100);
+        });
     });
 
     it('acquire timeout', function (done) {
@@ -287,7 +222,7 @@ describe('ConnectionPool', function () {
             'SELECT 42';
 
             var request = new Request(command, function (err, rowCount) {
-                assert(false);
+                assert(!!err);
             });
 
             request.on('row', function (columns) {
